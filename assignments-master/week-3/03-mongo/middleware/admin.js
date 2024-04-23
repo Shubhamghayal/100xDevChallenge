7ff9ffe5efbc787b3d33c563c03619372c301eec
Shubham <shubhamghayal009@gmail.com>
2024-04-23 12:53:40 +0530

    course_selling_backend_assignment

--- assignments-master/week-3/03-mongo/middleware/admin.js
+++ assignments-master/week-3/03-mongo/middleware/admin.js
@@ -1,5 +1,25 @@
-// Middleware for handling auth
+const {Admin}=require("../db")
+
+// Middleware forconst handling auth
 function adminMiddleware(req, res, next) {
+    const username=req.headers.username;
+    const password=req.headers.password
+
+    Admin.findOne({
+        username:username,
+        password:password
+    })
+    .then(function(value){
+        if(value){
+            next()
+        }else{
+            res.status(403).json({
+                msg:"user doesn't exist"
+            })
+        }
+    })
+
+
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
 }
