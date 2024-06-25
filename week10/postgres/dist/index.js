82f7a2ef8c58be4c85acd3ea28b90b1500a1b17f
Shubham <shubhamghayal009@gmail.com>
2024-06-26 03:54:32 +0530

    add week10

--- week10/postgres/dist/index.js
+++ week10/postgres/dist/index.js
@@ -1,9 +1,36 @@
 "use strict";
-function isLegal(user) {
-    if (user.age > 18) {
-        return true;
-    }
-    else {
-        return false;
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+const client = new pg_1.Client({
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    user: 'postgres',
+    password: 'Shubham@2160',
+});
+function insertUsersTable() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield client.connect(); // Ensure client connection is established
+            const insertQuery = "INSERT INTO users (username, email, password) VALUES ('sham', 'sham3@example.com', 'shamssword');";
+            const res = yield client.query(insertQuery);
+            console.log('Insertion success:', res); // Output insertion result
+        }
+        catch (err) {
+            console.error('Error during the insertion:', err);
+        }
+        finally {
+            yield client.end(); // Close the client connection
+        }
+    });
 }
+insertUsersTable();
--- week10/postgres/src/index.ts
+++ week10/postgres/src/index.ts
@@ -1,17 +1,25 @@
-interface User{
-    name:string,
-    age:number,
+import { Client } from 'pg'
+ 
 
 
+    const client = new Client({
+        host: 'localhost',
+        port: 5432,
+        database: 'postgres',
+        user: 'postgres',
+        password: 'Shubham@2160',
+      })
+    async function insertUsersTable(){try {
+    await client.connect(); // Ensure client connection is established
+    const insertQuery = "INSERT INTO users (username, email, password) VALUES ('sham', 'sham3@example.com', 'shamssword');";
+    const res = await client.query(insertQuery);
+    console.log('Insertion success:', res); // Output insertion result
+  } catch (err) {
+    console.error('Error during the insertion:', err);
+  } finally {
+    await client.end(); // Close the client connection
+  }
 }
 
-function isLegal(user:User){
-    if (user.age>18){
-        return true
+insertUsersTable()
 
-    }else{
-        return false
-    }
-    
-
-}
