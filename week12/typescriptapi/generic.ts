695d4f8fd77e998269fd54a0ddf4d841c0162638
Turbobot <turbobot@vercel.com>
2024-07-07 19:39:07 +0530

    feat(create-turbo): apply official-starter transform

---
+++ week12/typescriptapi/generic.ts
@@ -0,0 +1,16 @@
+// function getproduct <T, >(product:T[]):T{
+// return product[3]
+// }
+
+// console.log(getproduct([5,7,9,1,3]))
+
+
+// arrow function
+
+// const items=<T,>(item:T[]):T=>{
+//     return item[3]
+// }
+
+// console.log(items([7,8,6,1,3,4,5]))
+
+
